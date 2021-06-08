afa4e8b5cce5f255e4edea5daca249afbd68b782
abdullah_khan123 <sardarbhai6@gmail.com>
2021-06-08 13:24:32 +0530

    refractor codes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,10 @@
 import React from 'react';
-import soduko from 'sudoku';
 import Header from './components/header';
 import Board from './components/board';
 import './index.css';
 import './app.css';
 
-function generateSoduko() {
-  const raw = soduko.makepuzzle();
-  const rawResult = soduko.solvepuzzle(raw);
-  let emptyCells = 0;
-  const rows = [];
-
-  for (let i = 0; i < 9; i++) {
-    const row = { index: i, cols: [] };
-    for (let j = 0; j < 9; j++) {
-      const value = (raw[i * 9 + j]);
-      if (value === null) emptyCells++;
-      const col = {
-        row: i,
-        col: j,
-        value: value !== null ? value : "",
-        readOnly: value !== null ? true : false,
-        conflict: false
-      }
-      row.cols.push(col);
-    }
-    rows.push(row);
-  }
-  return { rows, rawResult, emptyCells, remEmptyCells: emptyCells };
-}
-
-function checkConflict(solved, cell) {
-  const correctValue = solved[cell.row * 9 + cell.col];
-  if (correctValue === cell.value) {
-    return false;
-  }
-  return true;
-}
-
-function trackEmptyCells(emptyCells, remEmptyCells, isCellValueEmpty) {
-  //update remaining empty cells value
-  if (isCellValueEmpty) {
-    let newEmptyCells = remEmptyCells + 1;
-    remEmptyCells = newEmptyCells <= emptyCells ? newEmptyCells : remEmptyCells;
-  } else {
-    remEmptyCells -= 1;
-  }
-  return remEmptyCells;
-}
+import {generateSoduko, checkConflict, trackEmptyCells} from './lib/sodukoBoard';
 
 class App extends React.Component {
   constructor(props) {
@@ -65,8 +22,7 @@ class App extends React.Component {
     rows[cell.row].cols[cell.col].value = cell.value;
     rows[cell.row].cols[cell.col].conflict = conflict;
     rows[cell.row].cols[cell.col].readOnly = isCellValueEmpty ? false : !conflict;
-    console.log(rows[cell.row].cols[cell.col])
-    
+
     remEmptyCells = trackEmptyCells(emptyCells, remEmptyCells, isCellValueEmpty)
 
     this.setState({
---
+++ src/lib/sodukoBoard.js
@@ -0,0 +1,44 @@
+import soduko from 'sudoku';
+
+export function generateSoduko() {
+    const raw = soduko.makepuzzle();
+    const rawResult = soduko.solvepuzzle(raw);
+    let emptyCells = 0;
+    const rows = [];
+
+    for (let i = 0; i < 9; i++) {
+        const row = { index: i, cols: [] };
+        for (let j = 0; j < 9; j++) {
+            const value = (raw[i * 9 + j]);
+            if (value === null) emptyCells++;
+            const col = {
+                row: i,
+                col: j,
+                value: value !== null ? value : "",
+                readOnly: value !== null ? true : false,
+                conflict: false
+            }
+            row.cols.push(col);
+        }
+        rows.push(row);
+    }
+    return { rows, rawResult, emptyCells, remEmptyCells: emptyCells };
+}
+
+export function checkConflict(solved, cell) {
+    const correctValue = solved[cell.row * 9 + cell.col];
+    if (correctValue === cell.value) {
+        return false;
+    }
+    return true;
+}
+
+export function trackEmptyCells(emptyCells, remEmptyCells, isCellValueEmpty){
+    if (isCellValueEmpty) {
+        let newEmptyCells = remEmptyCells + 1;
+        remEmptyCells = newEmptyCells <= emptyCells ? newEmptyCells : remEmptyCells;
+    } else {
+        remEmptyCells -= 1;
+    }
+    return remEmptyCells;
+}
