1ae415f6b5b5eb22f05ee6d8f76755e06f463936
abdullah_khan123 <sardarbhai6@gmail.com>
2021-06-08 09:42:23 +0530

    fixed checker

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import React from 'react';
 
 function generateSoduko() {
   const raw = soduko.makepuzzle();
+  const rawResult = soduko.solvepuzzle(raw);
   const rows = [];
 
   for (let i = 0; i < 9; i++) {
@@ -16,55 +17,39 @@ function generateSoduko() {
       const col = {
         row: i,
         col: j,
-        value: value !== null ? value + 1 : "",
+        value: value !== null ? value : "",
         readOnly: value ? true : false,
-        conflict:false
+        conflict: false
       }
       row.cols.push(col);
     }
     rows.push(row);
   }
-  return rows;
+  return { rows, rawResult };
 }
 
-function checkConflict(rows, cell) {
-  let conflict = false;
-  const cols = rows[cell.row].cols;
-  //vertical check
-  //same row different col
-  for (let i = 0; i < cols.length; i++) {
-    if (i === cell.col) continue;
-    if (cols[i].value === cell.value) {
-      conflict = true;
-      break;
-    }
+function checkConflict(solved, cell) {
+  const correctValue = solved[cell.row * cell.col];
+  if (correctValue === cell.value) {
+    return false;
   }
-  if(conflict) return conflict;
-  //same col different row
-  for(let j = 0; j < 9; j++) {
-    const col = rows[j].cols[cell.col];
-    if((cell.row === j) && col.col === cell.col) continue;
-    if(col.value === cell.value) {
-      conflict = true;
-      break;
-    }
-  }
-  return conflict;
+  return true;
 }
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      rows: generateSoduko(),
+      soduko: generateSoduko(),
     }
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange = (cell) => {
-    const newRowState = this.state.rows;
+    const newRowState = this.state.soduko.rows;
+    const solvedPuzzle = this.state.soduko.rawResult;
     newRowState[cell.row].cols[cell.col].value = cell.value;
-    newRowState[cell.row].cols[cell.col].conflict = checkConflict(newRowState, cell);
+    newRowState[cell.row].cols[cell.col].conflict = checkConflict(solvedPuzzle, cell);
     this.setState({
       rows: [...newRowState]
     })
@@ -80,7 +65,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
-        <Board rows={this.state.rows} onChange={this.handleChange} />
+        <Board rows={this.state.soduko.rows} onChange={this.handleChange} />
         <button className="block m-auto p-2 bg-blue-600 text-white" onClick={this.resetSoduko}>reset</button>
       </div>
     );
